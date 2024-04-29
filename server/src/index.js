d838e140678f6d9728e25e643d6a9c9500445902
Hyeon Ji Kim <94117828+hyeon81@users.noreply.github.com>
2024-04-29 20:49:43 +0900

    add publish

--- server/src/index.js
+++
@@ -1,9 +0,0 @@
-import { ApolloServer } from "apollo-server";
-import { typeDefs } from "./schema.js";
-import { resolvers } from "./resolver.js";
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen({ port: 5000 }).then(({ url }) => {
-  console.log(`Running on ${url}`);
-});
